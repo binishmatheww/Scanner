9b0d2c95b1b9ed6c6db722192b44df02444b60d7
binishmatheww <binishmatheww@gmail.com>
2022-11-19 22:38:36 +0530

    Made changes to the welcome screen.

--- app/src/main/java/com/binishmatheww/scanner/views/screens/WelcomeScreen.kt
+++ app/src/main/java/com/binishmatheww/scanner/views/screens/WelcomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -50,8 +51,9 @@ fun WelcomeScreen(
                         end.linkTo(parent.end)
                     }
                     .size(200.dp),
-                painter = painterResource(id = R.drawable.document_icon),
+                painter = painterResource(id = R.drawable.cameraic),
                 contentDescription = LocalContext.current.getString(R.string.app_name),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
             )
 
         }
