3121e426b580dda8bfcc7d2b17b91ba49f8230f7
binishmatheww <binishmatheww@gmail.com>
2022-11-20 22:07:52 +0530

    Reintroduced fragment navigation as the compose navigation cannot pass serializables as far as I know.

--- app/src/main/java/com/binishmatheww/scanner/views/LauncherActivity.kt
+++ app/src/main/java/com/binishmatheww/scanner/views/LauncherActivity.kt
@@ -1,28 +1,12 @@
 package com.binishmatheww.scanner.views
 
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Toast
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.binishmatheww.scanner.R
-import com.binishmatheww.scanner.common.constants.Routes
-import com.binishmatheww.scanner.views.fragments.HomeFragment
-import com.binishmatheww.scanner.views.fragments.PdfEditorFragment
-import com.binishmatheww.scanner.views.fragments.SplashScreenFragment
-import com.binishmatheww.scanner.views.screens.CameraScreen
-import com.binishmatheww.scanner.views.screens.HomeScreen
-import com.binishmatheww.scanner.views.screens.WelcomeScreen
 import com.binishmatheww.scanner.views.utils.clearTemporaryLocation
 import com.binishmatheww.scanner.views.utils.storageLocation
 import com.binishmatheww.scanner.views.utils.temporaryLocation
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -30,62 +14,13 @@ class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
 
         lifecycleScope.launch {
 
             temporaryLocation(this@LauncherActivity)
 
             storageLocation(this@LauncherActivity)
-
-            delay(400)
-
-            window.setBackgroundDrawableResource(android.R.color.transparent)
-
-        }
-
-        setContent {
-
-            val navController = rememberNavController()
-
-            NavHost(
-                navController = navController,
-                startDestination = Routes.welcomeScreen,
-            ) {
-
-                composable(
-                    route = Routes.welcomeScreen
-                ) {
-
-                    WelcomeScreen(
-                        onComplete = {
-                            navController.popBackStack()
-                            navController.navigate(Routes.homeScreen)
-                        }
-                    )
-
-                }
-
-                composable(
-                    route = Routes.homeScreen
-                ) {
-
-                    HomeScreen(
-                        onCameraClick = {
-                            navController.navigate(Routes.cameraScreen)
-                        }
-                    )
-
-                }
-
-                composable(
-                    route = Routes.cameraScreen
-                ) {
-
-                    CameraScreen()
-
-                }
-
-            }
 
         }
 
