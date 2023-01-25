78259d7d0556ec83b977c80c73fce975935ff1b9
binishmatheww <binishmatheww@gmail.com>
2023-01-25 23:26:31 +0530

    Revamped the home screen. Some WIP.

--- app/src/main/java/com/binishmatheww/scanner/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/binishmatheww/scanner/views/fragments/HomeFragment.kt
@@ -1,36 +1,18 @@
 package com.binishmatheww.scanner.views.fragments
 
-import android.app.Activity.RESULT_OK
-import android.content.Context
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.compose.ui.platform.ComposeView
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.binishmatheww.scanner.R
-import com.binishmatheww.scanner.views.adapters.FilePickerAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.navigation.NavigationView
-import java.io.File
-import android.Manifest
-import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.ModalDrawer
@@ -41,77 +23,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.binishmatheww.scanner.R
 import com.binishmatheww.scanner.common.theme.AppTheme
-import com.binishmatheww.scanner.common.utils.*
+import com.binishmatheww.scanner.common.utils.getPdfFiles
+import com.binishmatheww.scanner.common.utils.hasExternalStoragePermissions
+import com.binishmatheww.scanner.common.utils.pdfFilesFromStorageLocation
+import com.binishmatheww.scanner.common.utils.requestExternalStoragePermissions
+import com.binishmatheww.scanner.models.PdfFile
+import com.binishmatheww.scanner.viewmodels.HomeViewModel
 
 
 class HomeFragment : Fragment() {
 
-    private lateinit var cameraActionButton: FloatingActionButton
-    private lateinit var editorActionButton: FloatingActionButton
-    private lateinit var homeRecyclerView: RecyclerView
-    private lateinit var homeLinearLayout: LinearLayout
-    private lateinit var filePickerAdapter: FilePickerAdapter
-
-    private var files = mutableStateListOf<Uri>()
-
-    private lateinit var toggle : ActionBarDrawerToggle
-
-    var drawerLayout : DrawerLayout? = null
-    var drawer : NavigationView? = null
-
-    private var outFile : File? = null
-
-    private lateinit var filePickerLauncher : ActivityResultLauncher<Intent>
-
-    private lateinit var documentCreatorLauncher : ActivityResultLauncher<Intent>
-
-    private lateinit var cameraPermissionLauncher : ActivityResultLauncher<String>
-
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        filePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if(result.resultCode == RESULT_OK){
-                result?.data?.data?.let { uri ->
-                    openEditor(uri)
-                }
-            }
-        }
-        documentCreatorLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                result?.data?.data?.let { uri ->
-                    requireActivity().contentResolver.openOutputStream(uri)?.let{ os ->
-                        Log.wtf("uri",uri.toString())
-                        outFile?.let {
-                            os.write(it.readBytes())
-                            os.close()
-                            outFile = null
-                        }
-                    }
-                }
-            }
-        }
-        cameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            if (isGranted) {
-                requireActivity().supportFragmentManager.beginTransaction().replace(R.id.navigationController, CameraFragment(), "camera").addToBackStack("camera").commitAllowingStateLoss()
-            } else {
-                Toast.makeText(requireContext(),"Please enable camera permission",Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+    private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         return ComposeView(context = layoutInflater.context).apply {
             setContent {
                 HomeScreen(
-                    pdfFiles = files,
+                    pdfFiles = homeViewModel.files,
                     onCameraClick =  {
 
                         activity
@@ -133,73 +73,35 @@ class HomeFragment : Fragment() {
                                 }
                             )
 
+                    },
+                    onFileClick = { pdfFile ->
+
+                        activity
+                            ?.findNavController(R.id.navigationController)
+                            ?.navigate(
+                                R.id.action_homeFragment_to_pdfEditorFragment,
+                                Bundle().apply {
+                                    putString(
+                                        "uri",
+                                        pdfFile.uri?.toString()
+                                    )
+                                }
+                            )
+
                     }
                 )
             }
         }
 
     }
-
-    override fun onViewCreated(layout: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(layout, savedInstanceState)
-
-        /*
-
-        cameraActionButton.setOnClickListener {
-            when (PERMISSION_GRANTED) {
-                checkSelfPermission(requireContext(),CAMERA) -> {
-                    requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fContainer, CameraFragment(), "camera").addToBackStack("camera").commitAllowingStateLoss()
-                }
-                else -> {
-                    cameraPermissionLauncher.launch(CAMERA)
-                }
-            }
-
-        }
-
-
-        editorActionButton.setOnClickListener{
-            Toast.makeText(requireContext(),"Add images, pdf or txt files to edit",Toast.LENGTH_SHORT).show()
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fContainer, PdfEditorFragment(), "pdfEditor").addToBackStack("pdfEditor").commitAllowingStateLoss()
-        }
-
-        filePickerAdapter = FilePickerAdapter(requireContext(), files, object :
-            OnFileClickListener {
-            override fun onClicked(position: Int) {
-                openEditor(files[position])
-            }
-
-            override fun onDownload(position: Int) {
-                outFile = files[position]
-                createDocument(files[position].name, TYPE_PDF, documentCreatorLauncher)
-            }
-        })
-
-        filePickerAdapter.setHasStableIds(true)
-        homeRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        homeRecyclerView.adapter = filePickerAdapter
-        homeRecyclerView.setItemViewCacheSize(30)
-
-
-        val aboutUs : Button = layout.findViewById(R.id.aboutUs)
-        aboutUs.setOnClickListener {
-            Toast.makeText(requireContext(),"B Scanner version 1.0",Toast.LENGTH_SHORT).show()
-        }*/
-
-
-
-
-
-    }
-
-
     override fun onResume() {
         super.onResume()
 
         activity?.apply {
 
             if(hasExternalStoragePermissions()){
-                files.addAll(getPdfFiles())
+                homeViewModel.files.clear()
+                homeViewModel.files.addAll(getPdfFiles())
             }
             else{
                 requestExternalStoragePermissions()
@@ -211,9 +113,10 @@ class HomeFragment : Fragment() {
 
     @Composable
     fun HomeScreen(
-        pdfFiles: SnapshotStateList<Uri>,
-        onCameraClick : () -> Unit,
-        onEditorClick: () -> Unit
+        pdfFiles: SnapshotStateList<PdfFile>,
+        onCameraClick: () -> Unit,
+        onEditorClick: () -> Unit,
+        onFileClick: (PdfFile) -> Unit
     ) {
 
         AppTheme.ScannerTheme {
@@ -271,11 +174,17 @@ class HomeFragment : Fragment() {
                         }.fillMaxSize(),
                         content = {
                             
-                            items(pdfFiles){pdfFile ->
+                            itemsIndexed(pdfFiles){ index, pdfFile ->
                                 
                                 PdfFilePreview(
-                                    modifier = Modifier.fillMaxSize(),
-                                    pdfFile = pdfFile
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(
+                                            top = if(index == 0) navigationBarHeight else 0.dp,
+                                            bottom = if(index == pdfFiles.lastIndex) navigationBarHeight else 0.dp,
+                                        ),
+                                    pdfFile = pdfFile,
+                                    onFileClick = onFileClick
                                 )
                                 
                             }
@@ -347,18 +256,22 @@ class HomeFragment : Fragment() {
     @Composable
     private fun PdfFilePreview(
         modifier: Modifier = Modifier,
-        pdfFile: Uri
+        pdfFile: PdfFile,
+        onFileClick: (PdfFile) -> Unit
     ){
         
         Row(
             modifier = modifier
+                .clickable {
+                    onFileClick.invoke(pdfFile)
+                }
         ) {
             
             Text(
                 modifier = Modifier
                     .fillMaxSize(),
                 color = MaterialTheme.colorScheme.primary,
-                text = pdfFile.toString()
+                text = pdfFile.displayName ?: "Unknown"
             )
             
         }
