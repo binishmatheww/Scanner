81755118e997ec7ab4a6b1a72ac3d91a5d1e476f
binishmatheww <binishmatheww@gmail.com>
2023-01-25 21:38:30 +0530

    Added Scanner application and configured coil cache.

---
+++ app/src/main/java/com/binishmatheww/scanner/common/Scanner.kt
@@ -0,0 +1,32 @@
+package com.binishmatheww.scanner.common
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+
+class Scanner : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+
+        return ImageLoader
+            .Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(
+                        cacheDir.resolve("coilCache")
+                    )
+                    .maxSizeBytes(10 * 1024 * 1024)
+                    .build()
+            }
+            .build()
+
+    }
+
+}
