52092aef9a8683f6548401bd594aafcb79257e39
binishmatheww <binishmatheww@gmail.com>
2023-01-27 23:42:36 +0530

    Made home viewmodel a hilt one.

--- app/src/main/java/com/binishmatheww/scanner/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/binishmatheww/scanner/viewmodels/HomeViewModel.kt
@@ -1,11 +1,19 @@
 package com.binishmatheww.scanner.viewmodels
 
 
+import android.app.Application
 import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.binishmatheww.scanner.models.PdfFile
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomeViewModel: ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val app: Application
+) : AndroidViewModel(app) {
 
     val files = mutableStateListOf<PdfFile>()
 
