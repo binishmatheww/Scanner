57ba174be3471cb77e95171571d4ce2fd0c79ff7
binishmatheww <binishmatheww@gmail.com>
2023-01-28 17:38:35 +0530

    Removed file field from the class as it can be obtained from uri using Uri.toFile() if the scheme is file.

--- app/src/main/java/com/binishmatheww/scanner/models/PdfFile.kt
+++ app/src/main/java/com/binishmatheww/scanner/models/PdfFile.kt
@@ -4,12 +4,11 @@ import android.net.Uri
 import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
-import java.io.File
 
+@Suppress("DEPRECATION")
 data class PdfFile(
     val uri: Uri? = null,
     val displayName: String? = null,
-    val file: File? = null,
 ) : Parcelable{
     constructor(parcel: Parcel) : this(
 
@@ -20,18 +19,11 @@ data class PdfFile(
             parcel.readParcelable(Uri::class.java.classLoader)
         },
         displayName = parcel.readString(),
-        file = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            parcel.readSerializable(File::class.java.classLoader, File::class.java)
-        }
-        else {
-            parcel.readSerializable() as File?
-        }
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeParcelable(uri, flags)
         parcel.writeString(displayName)
-        parcel.writeSerializable(file)
     }
 
     override fun describeContents(): Int {
