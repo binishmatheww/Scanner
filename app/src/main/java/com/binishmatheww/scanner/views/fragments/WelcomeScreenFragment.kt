c6b9ff687e85ae6ab7e42ab483ecc107a6952567
binishmatheww <binishmatheww@gmail.com>
2023-01-29 19:03:55 +0530

    Fixed the user getting stuck on the splash screen if they pause and resume the screen.

--- app/src/main/java/com/binishmatheww/scanner/views/fragments/WelcomeScreenFragment.kt
+++ app/src/main/java/com/binishmatheww/scanner/views/fragments/WelcomeScreenFragment.kt
@@ -1,6 +1,8 @@
 package com.binishmatheww.scanner.views.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
@@ -22,7 +23,6 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.binishmatheww.scanner.R
 import com.binishmatheww.scanner.common.theme.AppTheme
-import kotlinx.coroutines.delay
 
 class WelcomeScreenFragment : Fragment() {
 
@@ -31,30 +31,29 @@ class WelcomeScreenFragment : Fragment() {
 
         return ComposeView(context = layoutInflater.context).apply {
             setContent {
-                WelcomeScreen {
-                    activity?.findNavController(R.id.navigationController)?.apply {
-                        navigate(R.id.action_welcomeScreenFragment_to_homeFragment)
-                    }
-                }
+                WelcomeScreen()
             }
         }
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                activity?.findNavController(R.id.navigationController)?.apply {
+                    navigate(R.id.action_welcomeScreenFragment_to_homeFragment)
+                }
+            }, 2000
+            )
+
+    }
+
     @Composable
-    private fun WelcomeScreen(
-        onComplete: () -> Unit
-    ) {
+    private fun WelcomeScreen() {
 
         AppTheme.ScannerTheme {
-
-            LaunchedEffect(
-                key1 = true,
-                block = {
-                    delay(2000)
-                    onComplete.invoke()
-                }
-            )
 
             ConstraintLayout(
                 modifier = Modifier
@@ -76,7 +75,7 @@ class WelcomeScreenFragment : Fragment() {
                         }
                         .size(200.dp),
                     painter = painterResource(id = R.drawable.cameraic),
-                    contentDescription = LocalContext.current.getString(R.string.app_name),
+                    contentDescription = LocalContext.current.getString(R.string.appName),
                     colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
                 )
 
