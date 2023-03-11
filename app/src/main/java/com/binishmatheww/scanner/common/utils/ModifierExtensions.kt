56a49207910fe4650c1fa3baf34d02a4b6554a30
binishmatheww <binishmatheww@gmail.com>
2023-03-11 18:23:58 +0530

    Added noRippleClickable() and noRippleCombinedClickable().

--- app/src/main/java/com/binishmatheww/scanner/common/utils/ModifierExtensions.kt
+++ app/src/main/java/com/binishmatheww/scanner/common/utils/ModifierExtensions.kt
@@ -1,14 +1,53 @@
 package com.binishmatheww.scanner.common.utils
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.toArgb
 
+
+/**
+ * Modifier extensions
+ */
+
+inline fun Modifier.noRippleClickable(
+    crossinline onClick : () -> Unit
+) = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+        onClick = {
+            onClick.invoke()
+        }
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+inline fun Modifier.noRippleCombinedClickable(
+    crossinline onClick : () -> Unit,
+    crossinline onLongClick : () -> Unit
+) = composed {
+    combinedClickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+        onClick = {
+            onClick.invoke()
+        },
+        onLongClick = {
+            onLongClick.invoke()
+        }
+    )
+}
 fun Modifier.drawColoredShadow(
     color: Color,
     alpha: Float = 0.2f,
