2ebe79e5ab2b6b135b6091a7f30f215dce2eb6d2
binishmatheww <binishmatheww@gmail.com>
2023-03-11 18:24:57 +0530

    Added new functions and renamed a property.

--- app/src/main/java/com/binishmatheww/scanner/models/PdfFile.kt
+++ app/src/main/java/com/binishmatheww/scanner/models/PdfFile.kt
@@ -4,31 +4,37 @@ import android.net.Uri
 import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.core.net.toFile
 
 @Suppress("DEPRECATION")
 data class PdfFile(
-    val uri: Uri? = null,
-    val displayName: String? = null,
+    val uri: Uri,
+    val name: String? = if(uri.scheme == "file") uri.toFile().name else null,
+    val filterSeed: Float = 100f
 ) : Parcelable{
     constructor(parcel: Parcel) : this(
-
         uri = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            parcel.readParcelable(Uri::class.java.classLoader, Uri::class.java)
+            parcel.readParcelable(Uri::class.java.classLoader, Uri::class.java) ?: Uri.EMPTY
         }
         else {
-            parcel.readParcelable(Uri::class.java.classLoader)
+            parcel.readParcelable(Uri::class.java.classLoader) ?: Uri.EMPTY
         },
-        displayName = parcel.readString(),
+        name = parcel.readString(),
+        filterSeed = parcel.readFloat(),
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeParcelable(uri, flags)
-        parcel.writeString(displayName)
+        parcel.writeString(name)
+        parcel.writeFloat(filterSeed)
     }
 
     override fun describeContents(): Int {
         return 0
     }
+
+    fun isFile(): Boolean = uri.scheme == "file"
+    fun delete(): Boolean = if(uri.scheme == "file") uri.toFile().delete() else false
 
     companion object CREATOR : Parcelable.Creator<PdfFile> {
         override fun createFromParcel(parcel: Parcel): PdfFile {
